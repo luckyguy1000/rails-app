416e8f43824ef6a4058c8376d64d2de932413aae
luckyguy1000 <smallcrazymonkey@outlook.com>
2020-09-10 12:40:55 +0300

    Fix specs
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,7 +4,6 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get welcome_index_url
     assert_response :success
-    assert_select "body", "You're on Rails!"
+    assert_select "h2", "You're on Rails!"
   end
-
 end
