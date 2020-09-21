e4e8e5bc4e1a69449f47a0fde8db9c618e8ed47c
luckyguy1000 <smallcrazymonkey@outlook.com>
2020-09-21 16:43:04 +0300

    Add db seeds
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+if Rails.env.development?
+  def random_content
+    case rand(3)
+    when 1
+      Faker::Lorem.question
+    when 2
+      Faker::Lorem.paragraph(sentence_count: rand(30) + 1)
+    else
+      Faker::Lorem.sentence
+    end
+  end
+
+  10.times do
+    post = Post.create!(title: Faker::Lorem.sentence, description: random_content)
+
+    rand(10).times do
+      post.comments.create(content: random_content)
+    end
+  end
+end
